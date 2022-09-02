6134c26b42cc02b6d6ae2d681efe36657a70d2ac
Phuc Pham <phameeek@gmail.com>
2022-09-02 21:01:23 +0200

    Fix function playRound Previously in some cases the function returned undefined value

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 function getComputerChoice() {
     if (Math.floor(Math.random()*3) === 0) {
         return "Rock";
@@ -8,22 +7,63 @@ function getComputerChoice() {
         return "Scissor";
     }
 }
+/*
+console.log(getComputerChoice());
+*/
 
 // Single round of Rock Paper Scissors
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock") {
-        return "You won! Paper beats Rock";
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Scissor") {
-        return "You lose! Scissor beats Paper";
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "Paper") {
-        return "You lose! Paper beats Rock";
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "Scissor") {
-        return "You won! Rock beats Scissor";
-    } else if (playerSelection.toLowerCase() === "scissor" && computerSelection === "Rock") {
-        return "You lose! Rock beats Scissor";
-    } else if (playerSelection.toLowerCase() === "scissor" && computerSelection === "Rock") {
-        return "You won! Scissor beats Paper";
-    } else {
-        return "Draw";
+        switch (true) {
+
+            case playerSelection.toLowerCase() === computerSelection.toLowerCase():
+                return "Draw";
+                break;       
+            case playerSelection.toLowerCase() === "paper" && computerSelection === "Rock":
+                return "You won! Paper beats Rock";
+                break;
+            case playerSelection.toLowerCase() === "paper" && computerSelection === "Scissor":
+                return "You lose! Scissor beats Paper";
+                break;         
+            case playerSelection.toLowerCase() === "rock" && computerSelection === "Paper":
+                return "You lose! Paper beats Rock";
+                break;
+            case playerSelection.toLowerCase() === "rock" && computerSelection === "Scissor":
+                return "You won! Rock beats Scissor";
+                break;
+            case playerSelection.toLowerCase() === "scissor" && computerSelection === "Rock":
+                return "You lose! Rock beats Scissor";
+                break;
+            case playerSelection.toLowerCase() === "scissor" && computerSelection === "Rock":
+                return "You won! Scissor beats Paper";
+                break;       
+        }
+    }
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+
+/*
+const computerMove = getComputerChoice();
+
+
+function Game() {
+    for (let i = 0; i < 5; i++) {
+        const playerMove = prompt("Enter a rock, paper or scissor.");
+        const computerMove = getComputerChoice();
+        console.log(playRound(playerMove, computerMove));
+        
+        const score = 0;
+        const result = playRound();
+        if (result === "You won! Paper beats Rock" || result === "You won! Rock beats Scissor" || result === "You won! Scissor beats Paper") {
+            score++;
+            console.log(score);
+        }
+        
+
     }
 }
+
+Game();
+*/
